e3698ad145bad0641d0b0cc50fe895a3330eb593
AMAN BANSAL <amankumarbansal05@gmail.com>
2025-09-22 15:03:27 +0530

    Add files via upload

---
+++ amangithub/Day5/index.js
@@ -0,0 +1,6 @@
+console.log("Welcome to the 5th day of 100 Days of Code!");
+function greet(name) {
+  return "Hello, " + name;
+}
+console.log(greet("Alice"));
+console.log(greet("Bob"));
---
+++ amangithub/Day6/6.js
@@ -0,0 +1,15 @@
+// (() => {
+//     var x = y = 6;
+// }) ();
+// console.log(x);
+// console.log(y);
+// console.log(typeof x);
+// console.log(typeof y);
+// let a = 23 - "23" + true + false - true + 23 + (-23);
+// console.log(a)
+// console.log(typeof a);
+// console.log(0.1 + 0.2 == 0.3);
+// console.log(0.1 + 0.2);
+function changeColor(color) {
+    document.getElementById("bulb").style.backgroundColor = color;
+}
